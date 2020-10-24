6ff83d90dc0a6c0b9d2e856009d015dfd0eb39ff
Vsevolod <lavrow.vs@gmail.com>
2020-10-24 17:53:17 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (var i = 0; i < str.length - 1; i++) {
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            if (
+                str[i] == bracketsConfig[j][0] &&
+                str[i + 1] == bracketsConfig[j][1]
+            ) {
+                str = str.substr(0, i) + str.substr(i + 2, str.length - 1);
+                i -= 2;
+            }
+        }
+    }
+    return str.length > 0 ? false : true;
+};
